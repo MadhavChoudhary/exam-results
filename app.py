c4650fa89661f7edc7b34880f9b767e76485707a
MadhavChoudhary <madhavtummala@icloud.com>
2020-06-30 14:06:15 +0530

    simple login complete

--- app.py
+++ app.py
@@ -3,15 +3,27 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
-options.headless = True
+options.headless = False
 
 driver = webdriver.Firefox(options=options)
 
 def login(regno, dob):
 	# to try to login the results page with the given
 	# registration number and date of birth
+	return
 
-def iterate():
-	# iterate through possible regno and dob
-	# collect data first to get some information
-	# about boundaries
+driver.get("http://cloudportal.sathyabama.ac.in/sist_results_may_2020/login.php")
+
+regno = "39111041"
+dob = "20/01/2002"
+
+regfield = driver.find_element_by_name("regno")
+dobfield = driver.find_element_by_name("dob")
+loginbtn = driver.find_element_by_id("btnLogin")
+
+regfield.send_keys(regno)
+dobfield.send_keys(dob)
+loginbtn.click()
+
+# driver.close()
+# exit()
